a8836208308d4a02c8d35a8f729f37e33bd60769
Anil Kulkarni <anil.kulkarni@outlook.com>
2019-03-11 00:25:28 -0700

    Implement the basic version

--- lib/plug_machine_token.ex
+++ lib/plug_machine_token.ex
@@ -1,18 +1,56 @@
 defmodule PlugMachineToken do
-  @moduledoc """
-  Documentation for PlugMachineToken.
-  """
+  alias JOSE.{JWK, JWS, JWT}
+  import Plug.Conn
 
-  @doc """
-  Hello world.
+  @jws JWS.from_map({%{alg: :jose_jws_alg_hmac}, %{"alg" => "HS256", "typ" => "JWT"}})
+  @algorithms ["HS256"]
 
-  ## Examples
+  def init(options) do
+    callback = Keyword.fetch!(options, :get_issuer_secret)
+    %{get_issuer_secret_fn: callback}
+  end
 
-      iex> PlugMachineToken.hello()
-      :world
+  def call(conn, %{get_issuer_secret_fn: get_issuer_secret_fn}) do
+    with {:ok, auth_header} <- get_authorization(conn),
+         {:ok, issuer} <- get_unverified_issuer(auth_header),
+         {:ok, issuer_secret} <- get_issuer_secret_fn.(issuer),
+         :ok <- validate_issuer_secret(issuer_secret),
+         jwk <- JWK.from_oct(issuer_secret),
+         {:ok, _jwt} <- validate_authorization(auth_header, jwk: jwk, issuer: issuer)
+         do
+        conn
+      else
+        {:error, error} -> send_resp(conn, 403, Atom.to_string(error)) |> halt()
+    end
+  end
 
-  """
-  def hello do
-    :world
+  defp get_authorization(conn) do
+    case get_req_header(conn, "authorization") do
+      ["Authorization: Bearer " <> value] -> {:ok, value}
+      [_value] -> {:error, :authorization_header_not_bearer}
+      [_value | _values] -> {:error, :too_many_authorization_headers}
+      _ -> {:error, :missing_authorization_header}
+    end
+  end
+
+  defp get_unverified_issuer(auth_header) do
+    try do
+      case JWT.peek(auth_header) do
+        %JWT{fields: %{"iss" => issuer}} -> {:ok, issuer}
+        _ -> {:error, :invalid_authorization_header}
+      end
+    rescue
+      _e -> {:error, :invalid_authorization_header}
+    end
+  end
+
+  defp validate_issuer_secret(<<_rest::size(256)>>), do: :ok
+  defp validate_issuer_secret(_), do: {:error, :invalid_issuer_secret}
+
+  defp validate_authorization(auth_header, jwk: jwk, issuer: issuer) do
+    case JWT.verify_strict(jwk, @algorithms, auth_header) do
+      {true, %JWT{fields: %{"iss" => ^issuer}} = jwt, @jws} -> {:ok, jwt}
+      _ -> {:error, :bad_signature}
+    end
   end
 end
--- test/plug_machine_token_test.exs
+++ test/plug_machine_token_test.exs
@@ -1,8 +1,46 @@
 defmodule PlugMachineTokenTest do
-  use ExUnit.Case
-  doctest PlugMachineToken
+  alias JOSE.{JWK, JWS, JWT}
+  use ExUnit.Case, async: true
+  use Plug.Test
 
-  test "greets the world" do
-    assert PlugMachineToken.hello() == :world
+  @key <<1::256>>
+
+  test "Passes without affecting the conn when the machine key is valid" do
+    callback = fn "my_service" -> {:ok, @key} end
+    auth_header = create_auth_header("my_service", @key)
+    conn = create_conn(auth_header)
+    assert ^conn = get(conn, callback)
+  end
+
+  test "halts if the signature is invalid" do
+    wrong_key = <<0::256>>
+    callback = fn "my_service" -> {:ok, wrong_key} end
+    auth_header = create_auth_header("my_service", @key)
+    conn = create_conn(auth_header)
+    conn = get(conn, callback)
+    assert 403 = conn.status
+    assert "bad_signature" = conn.resp_body
+    assert true = conn.halted
+  end
+
+  defp create_conn(auth_header) do
+    conn(:get, "/")
+    |> put_req_header("authorization", auth_header)
+  end
+
+  defp get(conn, callback) do
+    opts = PlugMachineToken.init(get_issuer_secret: callback)
+    PlugMachineToken.call(conn, opts)
+  end
+
+  defp create_auth_header(issuer, secret) do
+    jwk = JWK.from_oct(secret)
+    body = %{"iss" => issuer}
+
+    token = JWT.sign(jwk, %{ "alg" => "HS256" }, body)
+    |> JWS.compact
+    |> elem(1)
+
+    "Authorization: Bearer " <> token
   end
 end
