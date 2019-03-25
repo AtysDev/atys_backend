329274c38aca1b6a53126ecbce508a15c3776cca
Anil Kulkarni <anil.kulkarni@outlook.com>
2019-03-24 20:46:56 -0700

    Don't allow the user to log in if they haven't confirmed their email

--- lib/auth/routes/login.ex
+++ lib/auth/routes/login.ex
@@ -12,18 +12,24 @@ defmodule Auth.Routes.Login do
     with {:ok, {email, password}} <- get_values(conn.body_params),
     {:ok, user} <- User.find(email: email),
     :ok <- User.validate_password(user, password),
+    :ok <- validate_confirmed(user),
     {:ok, token} <- get_login_token(user) do
       Conn.resp(conn, 200, token)
     else
       {:error, :missing_email_or_password} -> Conn.resp(conn, 400, "missing email or password")
       {:error, :email_not_found} -> Conn.resp(conn, 403, "Invalid email or password")
       {:error, :invalid_password} -> Conn.resp(conn, 403, "Invalid email or password")
+      {:error, :email_not_confirmed} -> Conn.resp(conn, 403, "Email not confirmed")
       {:error, :token_unavailable_failure} -> Conn.resp(conn, 503, "Server Unavailable")
       {:error, :token_server_failure} -> Conn.resp(conn, 500, "Internal error")
       {:error, _error} -> Conn.resp(conn, 500, "Internal error")
     end
   end
   def create(conn, _opts), do: conn
+
+
+  defp validate_confirmed(%User{confirmed?: true}), do: :ok
+  defp validate_confirmed(_user), do: {:error, :email_not_confirmed}
 
   defp get_login_token(%User{id: id}) do
     token_url = Application.get_env(:auth, :token_url)
