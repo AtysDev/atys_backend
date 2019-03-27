095a7acea6cd20eecc265623cbb64a4108325769
Anil Kulkarni <anil.kulkarni@outlook.com>
2019-03-27 16:57:14 -0700

    Move the environment to the application, so it works properly

---
+++ lib/atys_api/environment.ex
@@ -0,0 +1,36 @@
+defmodule AtysApi.Environment do
+  @services %{
+    token: 4000,
+    auth: 4001
+  }
+
+  def get(:prod) do
+    Enum.map(@services, fn {name, _} ->
+      env_name =
+        "#{Atom.to_string(name)}_url"
+        |> String.to_atom()
+
+      env_value =
+        "#{Atom.to_string(name)}_url"
+        |> String.upcase()
+        |> System.get_env()
+
+      if env_value == nil do
+        raise "Missing environment value for #{env_name}"
+      end
+
+      {env_name, env_value}
+    end)
+  end
+
+  def get(_any) do
+    Enum.map(@services, fn {name, port} ->
+      env_name =
+      "#{Atom.to_string(name)}_url"
+      |> String.to_atom()
+
+    {env_name, "http://localhost:#{port}"}
+    end)
+  end
+
+end
--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule AtysApi.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      env: AtysApi.Environment.get(Mix.env()),
     ]
   end
 
