9672f656c788d0d0bf4f97e6e1c9f072c7cd4860
Anil Kulkarni <anil.kulkarni@outlook.com>
2019-03-30 01:48:36 -0700

    Support getting a machine_key

--- lib/atys_api/service/secret.ex
+++ lib/atys_api/service/secret.ex
@@ -1,6 +1,6 @@
 defmodule AtysApi.Service.Secret do
   require AtysApi.Errors
-  alias AtysApi.Request
+  alias AtysApi.{Errors, Request}
 
   def create_machine_key(%{auth_header: auth_header, request_id: request_id, project_id: project_id, key: key}) do
     url = Application.get_env(:atys_api, :secret_url)
@@ -16,6 +16,24 @@ defmodule AtysApi.Service.Secret do
       ],
       method: :post,
       data: data
+    ]
+
+    Request.send(url, request_id, opts)
+  end
+
+  def get_machine_key(%{auth_header: auth_header, request_id: request_id, id: id}) do
+    url = Application.get_env(:atys_api, :secret_url)
+    |> URI.merge("/#{id}")
+    |> to_string()
+    |> URI.encode()
+
+    opts = [
+      headers: [
+        {"Authorization", auth_header}
+      ],
+      expected_failures: [
+        Errors.reason(:item_not_found)
+      ],
     ]
 
     Request.send(url, request_id, opts)
